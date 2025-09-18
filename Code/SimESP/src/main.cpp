6ddcff390b6c710e95fe979e3e47aed08f159eff
TheEinfachFelix <the.einfach.felix@gmail.com>
2025-09-18 21:29:38 +0200

    eod

--- Code/SimESP/src/main.cpp
+++ Code/SimESP/src/main.cpp
@@ -7,7 +7,7 @@
 #define GSM_RX  16
 #define GSM_TX  17
 #define GSM_RST 22
-#define GSM_Sleep 23
+#define GSM_delay 23
 
 Sim800L GSM(GSM_RX, GSM_TX);
 
@@ -18,21 +18,27 @@ char* Inhalt_SMS;
 
 void setup() {
   GSM.begin(); 			
-  //GSM.Debug = true; // Enable debug mode to see AT command responses in Serial Monitor
+  GSM.setDebugMode(true);
   Serial.begin(9600);
 
   String pin = "2250";
-
-  if (GSM.setPIN(pin)) Serial.println("Pin set");
-  else Serial.println("Pin NOT set");
-
+  if (GSM.checkStatus().indexOf("READY") == -1) {
+      if (GSM.setPIN(pin)) Serial.println("Pin set");
+      else Serial.println("Pin NOT set");
+  }
+  delay(100);
+  GSM.checkStatus();
   delay(8000); //wait for the module to be ready
   //
   Serial.println("get Operator"); //ping the module to check if it is ready
   Serial.println(GSM.getOperator());
+  delay(100);
+  Serial.println(GSM.signalQuality());
+  delay(100);
 	text="Testing Sms";  		//text for the message. 
 	number="+4916094842537"; 		//change to a valid number.
 	//error=GSM.sendSms(number,text);
+  //delay(100);
   Serial.println("Message sent"); //message sent
   Serial.println(error); //error is true if the message was sent, false if not.
 	GSM.getCallStatus();
@@ -41,11 +47,12 @@ void setup() {
   Serial.println("call"); //message sent
 	// OR 
 	//Sim800L.sendSms("+540111111111","the text go here")
-  //GSM.checkStatus();
+  GSM.checkStatus();
+  delay(100);
 }
 
 void loop() {
-  Inhalt_SMS = "GPS Daten"; // holt sich GPS Daten
+  Inhalt_SMS = "GPSDaten"; // holt sich GPS Daten
   GSM.sendSms(number, Inhalt_SMS); // Sendet die GPS Daten per SMS
   Serial.println(Inhalt_SMS); // Weils so schön ist, printet er die nochmal
   delay(300000); // 8 min warten
