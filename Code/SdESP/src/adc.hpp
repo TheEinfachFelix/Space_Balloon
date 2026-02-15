d788e0f57b416901e283284cfa0126bbba933270
TheEinfachFelix <The.Einfach.Felix@gmail.com>
2026-02-15 10:55:19 +0100

    fixed imports

--- Code/SdESP/src/adc.hpp
+++ Code/SdESP/src/adc.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <Arduino.h>
 #include "config.hpp"
-#include <Adafruit_ADS1x15.h>
+#include <Adafruit_ADS1X15.h>
 
 bool setUpADC(int SCL, int SDA);
 bool setUpADC();
--- Code/SdESP/src/spec.hpp
+++ Code/SdESP/src/spec.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <Arduino.h>
 #include "config.hpp"
-#include <SparkFun_AS7265x.h>
+#include <SparkFun_AS7265X.h>
 
 bool setUpSpec();
 bool setUpSpec(int scl, int sda);
